6053077532c475c2f07e44892d344d2fbb971ef9
Jacob Logan <jacob.maiola.logan@gmail.com>
2024-01-15 17:17:42 -0700

    add list to 7

--- scripts/convertToJSON.js
+++ scripts/convertToJSON.js
@@ -145,7 +145,7 @@ function writePullSheetCSV(pullData) {
     fs.writeFileSync(pullSheetLocation, csvLines.join('\n'), 'utf-8');
 }
 
-// AddInventoryToBox(6);
+// AddInventoryToBox(7);
 
 /**
  * Steps to pull cards
