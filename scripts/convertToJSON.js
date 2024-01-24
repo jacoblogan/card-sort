309656f6d844f13c144258947089c05100d07548
Jacob Logan <jacob.maiola.logan@gmail.com>
2024-01-24 03:36:23 -0700

    DU set box uploads 1-24

--- scripts/convertToJSON.js
+++ scripts/convertToJSON.js
@@ -145,7 +145,7 @@ function writePullSheetCSV(pullData) {
     fs.writeFileSync(pullSheetLocation, csvLines.join('\n'), 'utf-8');
 }
 
-// AddInventoryToBox(7);
+// AddInventoryToBox(8);
 
 /**
  * Steps to pull cards
