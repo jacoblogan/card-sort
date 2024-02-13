c84f15873bcb34d17ad7a849e2606352bb17dca9
Jacob Logan <jacob.maiola.logan@gmail.com>
2024-02-13 01:51:31 -0700

    add lotr to box 2

--- scripts/convertToJSON.js
+++ scripts/convertToJSON.js
@@ -145,7 +145,7 @@ function writePullSheetCSV(pullData) {
     fs.writeFileSync(pullSheetLocation, csvLines.join('\n'), 'utf-8');
 }
 
-// AddInventoryToBox(6);
+// AddInventoryToBox(2);
 
 /**
  * Steps to pull cards
