9ca300e997aa84da08729e1c08e6b806f871f38e
Jacob Logan <jacob.maiola.logan@gmail.com>
2024-03-30 11:18:01 -0700

    add NCC to box 11

--- scripts/convertToJSON.js
+++ scripts/convertToJSON.js
@@ -145,7 +145,7 @@ function writePullSheetCSV(pullData) {
     fs.writeFileSync(pullSheetLocation, csvLines.join('\n'), 'utf-8');
 }
 
-// AddInventoryToBox(12);
+// AddInventoryToBox(11);
 
 /**
  * Steps to pull cards
