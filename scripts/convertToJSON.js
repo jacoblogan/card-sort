0948b63cb256fe8ccce288232cd83e5fa125ce50
Jacob Logan <jacob.maiola.logan@gmail.com>
2025-11-14 18:19:38 -0700

    add to 168

--- scripts/convertToJSON.js
+++ scripts/convertToJSON.js
@@ -523,7 +523,7 @@ function generateShippingHP() {
   });
 }
 
-// AddInventoryToBox(20);
+// AddInventoryToBox(168);
 
 // generate add sheet for both tcgplayer and backlog
 // also updates the backlog and myData json files with the new inventory
